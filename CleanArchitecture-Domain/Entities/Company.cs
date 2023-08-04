a24be495754ccd41dff6d3be7e08cc0063336ad5
ABOLADE BOLU MARVELOUS <130442240+Marvelous-iAcademy@users.noreply.github.com>
2023-08-04 16:43:01 +0100

    Add files via upload
    Domain

---
+++ CleanArchitecture-Domain/Entities/Company.cs
@@ -0,0 +1,11 @@
+﻿namespace CleanArchitecture_Domain.Entities
+{
+    public class Company : BaseEntity
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Country { get; set; }
+        public ICollection<Employee> Employees { get; set; }
+
+    }
+}
---
+++ CleanArchitecture-Domain/Entities/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CleanArchitecture_Domain.Entities
+{
+    public class Employee : BaseEntity
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Position { get; set; }
+        [ForeignKey(nameof(Company))]
+        public string CompanyId { get; set; }
+        public Company Company { get; set; }
+    }
+}
